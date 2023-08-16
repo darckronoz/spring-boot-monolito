4a2bcd8cb79cc6fe2da260503a43e5c8a96d4a24
darckronoz <crispipe14@gmail.com>
2023-08-16 11:11:54 -0500

    pdf numero 2 ended

--- src/main/java/com/darck/biblioteca/controller/ListadoController.java
+++ src/main/java/com/darck/biblioteca/controller/ListadoController.java
@@ -1,12 +1,25 @@
 package com.darck.biblioteca.controller;
 
+import com.darck.biblioteca.domain.Libro;
+import com.darck.biblioteca.service.LibroService;
+import java.util.List;
+import org.springframework.ui.Model;    
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ListadoController {
+
+    private final LibroService libroService;
+
+    public ListadoController(LibroService libroService) {
+        this.libroService = libroService;
+    }
+
     @RequestMapping("/")
-    public String listarLibros() {
+    public String listarLibros(Model model) {
+        List<Libro> destacados = libroService.buscarDestacados();
+        model.addAttribute("ibros", destacados);
         return "listado";
     }
 }
